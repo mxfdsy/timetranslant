843945b9bcac33d15fa6c4d90c82f5b200f4ddbe
963559210@qq.com <963559210@qq.com>
2018-04-10 18:51:05 +0800

    到了写正则了

---
+++ src/TimeCase.java
@@ -0,0 +1,34 @@
+/**
+ * 需求：编写一个程序，要求用户输入24小时制的时间，然后显示12小时制的时间且加上AM,PM
+ * 如12:34表示12点34分。当小时或分钟数小于10时，均没有前导的零，如5:6表示5点零6分。
+ * Created by 没想法的岁月 on 2018/4/10.
+ */
+public class TimeCase {
+    public static void main(String[] args) {
+        changeTime("1:00");
+    }
+
+    private static void changeTime(String s) {
+        if (cheak(s)) {
+            String[] split = s.split(":");
+            int a = Integer.parseInt(split[0]);
+            if (a >= 12 && a != 24) {
+                int newa = a - 12;
+                String newStr = String.valueOf(newa) + ":" + split[1] + " PM";
+                System.out.println(newStr);
+            } else if (a == 24) {
+                System.out.println("00" + ":" + split[1] + " AM");
+            } else {
+                System.out.println(split[0] + ":" + split[1] + " AM");
+            }
+        } else {
+            System.out.println("请输入正确的24小时格式,例如 12:34表示12点34分");
+        }
+    }
+
+    private static boolean cheak(String s) {
+//         String regex ="^\b\w{6}[0-24];";
+//        return s.matches(regex);
+        return  true;
+    }
+}
